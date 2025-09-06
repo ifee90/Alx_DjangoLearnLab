81ddcb93f151a66571c4734505a6ce3fff0fe10b
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-09-07 00:15:36 +0100

    Task 2: FBV returns plain text list, CBV for library details, URLs configured

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from .models import Book, Library
@@ -6,9 +5,11 @@ from .models import Book, Library
 # Function-based view: List all books as plain text (ALX requirement)
 def list_books(request):
     books = Book.objects.all()
-    # Create a simple text list: "Title by Author"
-    output = "\n".join(f"{book.title} by {book.author.name}" for book in books)
-    return HttpResponse(output)
+    output_lines = []
+    for book in books:
+        output_lines.append(f"{book.title} by {book.author.name}")
+    output_text = "\n".join(output_lines)
+    return HttpResponse(output_text, content_type="text/plain")
 
 # Class-based view: Show details of a specific library (HTML template)
 class LibraryDetailView(DetailView):
