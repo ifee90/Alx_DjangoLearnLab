d6c8de6122fd62c257a9015121a510f59bf8cf17
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-09-06 23:52:01 +0100

    Task 2: Added FBV as plain text, CBV, templates, and URL configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.detail import DetailView
 from .models import Book, Library
 
-# Function-based view: List all books
+# Function-based view: List all books as plain text (ALX requirement)
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # Create a simple text list: "Title by Author"
+    output = "\n".join(f"{book.title} by {book.author.name}" for book in books)
+    return HttpResponse(output)
 
-from django.views.generic.detail import DetailView
-
-# Class-based view: Show details of a specific library
+# Class-based view: Show details of a specific library (HTML template)
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
