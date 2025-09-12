bfd356641b66903b3f03dab219de0a4d8bc94fe6
fifi-codes <ifeomaoguejiofor90@gmail.com>
2025-09-12 12:27:26 +0100

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import Book  # Make sure you're importing your Book model
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book, Author, Library, UserProfile
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')  # Removed publication_year
-    list_filter = ('author',)            # Removed publication_year
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
 
-admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Library)
+admin.site.register(UserProfile)
