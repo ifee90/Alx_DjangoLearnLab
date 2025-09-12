c923247298d0cf6d8b55a5700a2111c9cdaf8ae5
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-09-12 10:09:08 +0100

    Update CustomUser model and register CustomUserAdmin

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -1,20 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Book, Author, Library
+from .models import Book  # Make sure you're importing your Book model
 
-# ✅ Custom User Admin
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ('username', 'email', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active')
-    fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('date_of_birth', 'profile_photo')}),
-    )
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')  # Removed publication_year
+    list_filter = ('author',)            # Removed publication_year
 
-# ✅ Register models
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Library)
+admin.site.register(Book, BookAdmin)
