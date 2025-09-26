2619d2c3b2860b09601e7cb08168e798af52a1fc
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-09-27 00:56:30 +0100

    Fix tests to use self.client.login for test database authentication

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,48 +1,56 @@
 # api/test_views.py
-from django.urls import reverse
+
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Author, Book
+from django.urls import reverse
+from .models import Book, Author
 
 class BookAPITests(APITestCase):
+
     def setUp(self):
-        self.author = Author.objects.create(name="Chinua Achebe")
-        self.book1 = Book.objects.create(
-            title="Things Fall Apart", publication_year=1958, author=self.author
-        )
-        self.book2 = Book.objects.create(
-            title="No Longer at Ease", publication_year=1980, author=self.author
-        )
+        # Create test user and log in
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+
+        # Create test authors
+        self.author1 = Author.objects.create(name="Author One")
+        self.author2 = Author.objects.create(name="Author Two")
+
+        # Create test books
+        self.book1 = Book.objects.create(title="Book One", publication_year=2000, author=self.author1)
+        self.book2 = Book.objects.create(title="Book Two", publication_year=2005, author=self.author2)
 
     def test_book_list(self):
         url = reverse('book-list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue('title' in response.data[0])
-        self.assertTrue('publication_year' in response.data[0])
-        self.assertTrue('author' in response.data[0])
+        self.assertIn('Book One', str(response.data))
+        self.assertIn('Book Two', str(response.data))
 
     def test_book_create(self):
         url = reverse('book-list')
-        # Authenticate a dummy user for POST
-        self.client.force_authenticate(user=None)
         data = {
-            "title": "Arrow of God",
-            "publication_year": 1964,
-            "author": self.author.id
+            'title': 'Book Three',
+            'publication_year': 2010,
+            'author': self.author1.id
         }
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['title'], 'Book Three')
 
     def test_book_update(self):
         url = reverse('book-detail', args=[self.book1.id])
-        self.client.force_authenticate(user=None)
-        data = {"title": "Things Fall Apart - Updated"}
-        response = self.client.patch(url, data)
+        data = {
+            'title': 'Updated Book One',
+            'publication_year': 2001,
+            'author': self.author1.id
+        }
+        response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Updated Book One')
 
     def test_book_delete(self):
         url = reverse('book-detail', args=[self.book2.id])
-        self.client.force_authenticate(user=None)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
