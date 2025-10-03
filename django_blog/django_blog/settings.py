07e5f947029855236de11a66b3a4743332e7c557
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-10-03 15:00:43 +0100

    Fix: add USER and PORT to DATABASES for ALX checker

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',        # dummy database name
+        'USER': 'blog_user',      # dummy username
+        'PASSWORD': 'blog_pass',  # dummy password
+        'HOST': 'localhost',
+        'PORT': '5432',           # default PostgreSQL port
     }
 }
+
 
 
 # Password validation
