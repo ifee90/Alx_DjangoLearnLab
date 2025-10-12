349acd6d102cef42d913128bf6fd201b56b492c3
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-10-12 18:28:13 +0100

    Update like/unlike views to use get_or_create and generics.get_object_or_404

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import get_object_or_404
@@ -64,41 +64,14 @@ def like_post(request, pk):
     Allows a user to like a post.
     Prevents multiple likes by the same user.
     """
-    post = get_object_or_404(Post, pk=pk)
+    post = generics.get_object_or_404(Post, pk=pk)
     user = request.user
 
-    if Like.objects.filter(user=user, post=post).exists():
+    like, created = Like.objects.get_or_create(user=user, post=post)
+    if not created:
         return Response({'message': 'You have already liked this post.'})
-
-    Like.objects.create(user=user, post=post)
 
     # Create notification for the post author
     if post.author != user:
         Notification.objects.create(
             recipient=post.author,
-            actor=user,
-            verb='liked your post',
-            target=post
-        )
-
-    return Response({'message': 'Post liked successfully.'})
-
-
-# -----------------------------
-# Unlike a Post
-# -----------------------------
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def unlike_post(request, pk):
-    """
-    Allows a user to unlike a post they previously liked.
-    """
-    post = get_object_or_404(Post, pk=pk)
-    user = request.user
-
-    like = Like.objects.filter(user=user, post=post).first()
-    if not like:
-        return Response({'message': 'You have not liked this post.'})
-
-    like.delete()
-    return Response({'message': 'Post unliked successfully.'})
